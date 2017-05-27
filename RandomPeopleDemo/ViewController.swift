c48563b63b9aa488a0b64e6931b3a8a8aada1759
Boyko Andrey <_flanker_4@mail.ru>
2017-05-27 23:50:26 +0300

    Initial Commit

---
+++ RandomPeopleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomPeopleDemo
+//
+//  Created by Boyko Andrey on 5/27/17.
+//  Copyright © 2017 aboiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
